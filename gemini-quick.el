dc39798f383b1a7ee332b7f4c35c0844a9a81b02
Benni Schwerdtner <Benjamin.Schwerdtner@gmail.com>
2025-02-16 07:38:12 +0000

    Polish stuff

--- gemini-quick.el
+++ gemini-quick.el
@@ -22,41 +22,39 @@
 (require 'request)
 (require 's)
 
-(defcustom gemini-api-key
-  (let ((s))
-    (lambda ()
-      (setf
-       s
-       (or
-        (getenv "GEMINI_API_KEY")
-        (shell-command-to-string "pass gai/gemini-api-key-1")))))
+(defcustom gemini-api-key (lambda () (getenv "GEMINI_API_KEY"))
   "Function to get API key for gemini, called without arguments.")
 
-(defun quick-gemini-chat (arg)
+;; (defcustom gemini-output-buffer-function
+;;   (lambda ())
+;;   ""
+;;   )
+
+(defun gemini-quick-chat (arg)
   (interactive "P")
   (message "%s" arg))
 
 (defcustom gemini-default-model
-  "gemini-1.5-flash"
+  "gemini-2.0-flash"
+  ;; "gemini-1.5-flash"
   "Default model for gemini chat")
 
 (defvar gemini-model gemini-default-model)
-(defvar gemini-model
-  ;; "gemini-2.0-pro-exp-02-05"
-  ;; "gemini-2.0-flash-thinking-exp-01-21"
-  ;; "gemini-2.0-flash-lite-preview-02-05"
-  "gemini-2.0-flash" )
 
-(define-derived-mode gemini-chat-mode text-mode "Gemini Chat")
+(define-derived-mode gemini-quick-chat-mode text-mode "Gemini Chat")
 
 ;; keymap
-(defvar gemini-chat-mode-map
-  (let ((map (make-sparse-keymap)))
-    (keymap-set map "RET" #'gemini-chat-select-markdown-block)
-    map)
-  "Keymap for `gemini-chat-mode'.")
+(defvar gemini-quick-chat-mode-map '()
+  "Keymap for `gemini-quick-chat-mode'.")
 
-(defun quick-gemini-chat (arg)
+(setf
+ gemini-quick-chat-mode-map
+ (let ((map (make-sparse-keymap)))
+   (keymap-set map "RET" #'gemini-quick-select-markdown-block)
+   map))
+
+
+(defun gemini-quick-chat (arg)
   "Send buffer content (or region) to Gemini and display response in '*gemini-output*'.
 
 With a prefix argument ARG, prompts for a question to append to the buffer content
@@ -78,14 +76,23 @@ If there's an error, error details will also be shown in '*gemini-output*'."
                   (concat
                    "\n"
                    (read-string "Q: ")))))
-         (output-buffer (get-buffer-create "*gemini-output*" (s-trim (shell-command-to-string "date +%s"))))
+         (output-buffer (get-buffer-create
+                         (concat
+                          "*gemini-"
+                          "-"
+                          (buffer-name)
+                          "-"
+                          (s-trim
+                           (shell-command-to-string
+                            "date +%s")))))
          (api-key (funcall gemini-api-key))
          (url (concat
                "https://generativelanguage.googleapis.com/v1beta/models/"
                gemini-model
                ":generateContent?key="
                api-key)))
-    (message "gemmini chat request...")
+    (message
+     "gemmini chat request...")
     (request
       url
       :headers '(("Content-Type" . "application/json"))
@@ -95,35 +102,34 @@ If there's an error, error details will also be shown in '*gemini-output*'."
       :parser 'json-read
       :success (cl-function
                 (lambda (&key data &allow-other-keys)
-                  ;; (defvar thedata data)
                   (with-current-buffer
                       output-buffer
                     (erase-buffer)
-                    (gemini-chat-mode)
+                    (gemini-quick-chat-mode)
                     (insert
                      (alist-get
                       'text
-                      (car
-                       (mapcar
-                        #'identity
-                        (alist-get
-                         'parts
-                         (alist-get
-                          'content
-                          (car (mapcar
-                                #'identity
-                                (alist-get 'candidates data)))))))))
+                      (car (mapcar
+                            #'identity
+                            (alist-get
+                             'parts
+                             (alist-get
+                              'content
+                              (car (mapcar
+                                    #'identity
+                                    (alist-get 'candidates data)))))))))
                     (display-buffer
                      (current-buffer)))))
       :error (cl-function
               (lambda (&key data &allow-other-keys)
-                (with-current-buffer output-buffer
+                (with-current-buffer
+                    output-buffer
                   (erase-buffer)
                   (insert (format "%s" data))
                   (display-buffer
                    (current-buffer))))))))
 
-(defun gemini-chat-select-markdown-block ()
+(defun gemini-quick-select-markdown-block ()
   (interactive)
   (let ((beg (progn
                (re-search-backward "^```")
